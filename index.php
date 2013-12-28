3d2a1d16b3299ca03de76fad6a810e301c066259
Mike Qin <1mike12@gmail.com>
2013-12-28 03:03:19 -0500

    finished all row and column manipulation finished matlab output and formatting
    started auto expand field when focused
    started reworking output warnings

--- index.php
+++ index.php
@@ -14,11 +14,15 @@
     </head>
     <body>
         <header class="">
-            <img id="logo" src="images/shitty%20logo.png" alt="logo"/>
-            <div id="logoText">
-                Matrix Translator
-                <div id="logoSubTitle">making matrices a little less shitty</div>
-            </div>
+            <a href="index.php">
+                <img id="logo" src="images/shitty%20logo.png" alt="logo"/>
+                <div id="logoText">
+                    Matrix Translator
+                    <div id="logoSubTitle">making matrices a little less shitty</div>
+                </div>
+            </a>
+
+
             <ul>
                 <li><a href="">Matrix</a></li>
                 <li><a href="">About</a></li>
@@ -31,11 +35,12 @@
                     <div id="matrixWrapper">
 
                         <div id="matrixOptions" class="dropShadow">
-                            rows: <input type="text" /> columns: <input type="text" />
+                            rows: <input id="rowField" type="text" /> columns: <input id="colField" type="text" />
                             <input id="colPlus" type="button" value="col+"/>
                             <input id="colMinus" type="button" value="col-"/>
                             <input id="rowPlus" type="button" value="row+"/>
                             <input id="rowMinus" type="button" value="row-"/>
+                            <input id="clearAll" type="button" value="clear all"/>
                         </div>
 
                         <div id="matrixCanvas" class="dropShadow">
@@ -48,7 +53,12 @@
                                 <tr id="r2">
                                     <td><input id="r2c1" type="text" class=""/></td>
                                     <td><input id="r2c2" type="text" class=""/></td>
-                                    <td><input id="r1c3" type="text" class=""/></td>
+                                    <td><input id="r2c3" type="text" class=""/></td>
+                                </tr>
+                                <tr id="r3" style="display: table-row;">
+                                    <td><input id="r3c1" type="text" class=""></td>
+                                    <td><input id="r3c2" type="text" class=""></td>
+                                    <td><input id="r3c3" type="text" class=""></td>
                                 </tr>
                             </table>
                         </div>
@@ -59,24 +69,21 @@
                 </div>
                 <div id="outputColumn" class="col span_1_of_3">
                     <div class="dropShadow">
-                        <h3>WolframAlpha</h3>
-                        <div class="warning">not valid format yet<img src="images/info.png" alt="explanation"/></div>
-
+                        <div class="outputHeaderWrapper">
+                            <h3>WolframAlpha</h3>
+                            <span id="wolframWarning" class="warning"></span>
+                        </div>
                         <div class="outputFieldWrapper">
-                            <input type="text" value="{{1, 2}, {3, 4}, {5, 6}}">
-                            <div class="ellipsisContainer">...</div>
+                            <input type="text" value="">
                         </div>
 
                         <h3>MAT Lab</h3>
-                        <div class="warning"></div>
-                        <input type="text" value="[16 3 2 13; 5 10 11 8; 9 6 7 12; 4 15 14 1]">
+                        <input id="matLabOutput" type="text" value="">
 
                         <h3>Google</h3>
-                        <div class="warning"></div>
                         <input type="text">
 
                         <h3>LateX</h3>
-                        <div class="warning"></div>
                         <input type="text" value="begin{array}{ccc} a & b & c">
                     </div>
                 </div>
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    var matrix = $("#matrix");
+    var $matrixFields = $("#matrix input:text");
     //auto highlight fields when focused
     var autoHighlight = $('#outputColumn input:text, #matrix input:text');
 
@@ -29,30 +31,115 @@ $(document).ready(function() {
     }
 
     function colPlus($table) {
-        var rows = getRows($table);
-        var columns = getCols($table);
+        var cols = getCols($table);
 
         var $rowSet = $table.find("tr");
         for (var i = 0; i < $rowSet.length; i++) {
-            var $td = $(printTD(i + 1, columns + 1)).hide();
+            var $td = $(printTD(i + 1, cols + 1));
             $rowSet.eq(i).append($td);
-            $td.fadeIn(200);
         }
 
     }
     function colMinus($table) {
         var $TDSet = $table.find("tr").find("td:last");
         for (var i = 0; i < $TDSet.length; i++) {
-            $TDSet.fadeOut(200, function(){$TDSet.remove();});
+            $TDSet.remove();
         }
     }
+    function rowPlus($table) {
+        var rows = getRows($table);
+        var cols = getCols($table);
 
-    var matrix = $("#matrix");
+        //add row with class
+        $table.append('<tr id="r' + (rows + 1) + '"></tr>');
+        var $lastRow = $table.find("tr:last");
+
+        for (var i = 0; i < cols; i++) {
+            var td = printTD(rows + 1, i + 1);
+            $lastRow.append(td);
+        }
+    }
+    function rowMinus($table) {
+        var tr = $table.find("tr:last");
+        tr.remove();
+    }
+    function updateFields() {
+        var rows = getRows(matrix);
+        var cols = getCols(matrix);
+        $("#rowField").val(rows);
+        $("#colField").val(cols);
+    }
+
+    function generateMatrixArray($table) {
+        var matrix = new Array();
+        var rows = getRows($table);
+        var cols = getCols($table);
+
+        for (var r = 0; r < rows; r++) {
+            matrix[r] = new Array();
+            for (var c = 0; c < cols; c++) {
+                var currentField = "#r" + (r + 1) + "c" + (c + 1);
+                matrix[r][c] = $(currentField).val();
+            }
+        }
+        return matrix;
+    }
+
+    function generateMatlab(array) {
+        var rows = array.length;
+        var cols = array[0].length;
+        var out = "[ ";
+        for (var r = 0; r < rows; r++) {
+            for (var c = 0; c < cols; c++) {
+                out += array[r][c] + " ";
+            }
+            //add semicolon unless it's the last row
+            if ((r + 1) < rows) {
+                out += "; ";
+            }
+        }
+        out += " ]";
+        return out;
+    }
+
     $("#colPlus").click(function() {
         colPlus(matrix);
+        updateFields();
     });
 
     $("#colMinus").click(function() {
         colMinus(matrix);
+        updateFields();
     });
+
+    $("#rowPlus").click(function() {
+        rowPlus(matrix);
+        updateFields();
+    });
+    $("#rowMinus").click(function() {
+        rowMinus(matrix);
+        updateFields();
+    });
+
+    $("#clearAll").click(function() {
+        $matrixFields.val("");
+    });
+
+    //generate matrix array for each focus out when inside a matrix field
+    $("#matrix input:text").focusout(function() {
+        var g = generateMatrixArray(matrix);
+        $("#matLabOutput").val(generateMatlab(g));
+        //wolfram
+        //latex
+    });
+
+    //autoexpand matrix field
+    var matrixFieldWidth=$("#matrix input:text").outerWidth();
+    $("#matrix input:text")
+            .focus(function() {
+                $(this).animate({width: "300px"}, 300);
+            })
+            .focusout(function() {
+                $(this).animate({width: matrixFieldWidth+"px"}, 300);
+            });
 });
