4a8fb23cfc3f7a3b48c143b8865592e28d3ada3e
Mike Qin <1mike12@gmail.com>
2013-12-30 22:28:55 -0500

    started work on about page

--- about.php
+++ about.php
@@ -7,10 +7,10 @@
             <div class="section group">
                 <div class="col span_3_of_3">
                     <div id="banner">
-                        <span id="bannerTitle">About Matrix Translator</span>
+                        <span class="h-Center">About Matrix Translator</span>
                     </div>
                     <div class="content">
-                        
+                        <h1 style="margin:auto; display:block; width:50%;">content</h1>
                     </div>
                 </div>
             </div>
