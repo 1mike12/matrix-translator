58125ce08df35b3ef0a64b96e7323d70467a7365
Mike Qin <1mike12@gmail.com>
2014-01-17 23:47:19 -0500

    commit before testing accordion
    todo:
    fix responsive on matrix page. widths broken again
    fix footer not goign to bottom on comments.php

--- about.php
+++ about.php
@@ -10,7 +10,7 @@
                         <h1>How it all started</h1>
                         <div>I got the inspiration to make the matrix translator while (surprise) taking a linear algebra class. In class, we had to use matlab, but we all used WolframAlpha when it was time to do homework since nobody had a matlab license. Between memorizing eigen' this and eigen' that, there was no room left in my brain to remember the finicky syntax between each program. I found myself spending 5-10 minutes every single time I did my homework to re-look up how to write the code.</div>
                         <h1 style="margin:auto; display:block;">Mike Qin </h1>
-                        <h3>fucntions</h3>
+                        <h3 >fucntions</h3>
                         <h3>matrix table</h3>
                         <h3>css , scss</h3>
                         <h3>fucntions</h3>
--- head.php
+++ head.php
@@ -5,7 +5,8 @@
     <!--qtip2 shtuff-->
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/qtip2/2.1.1/jquery.qtip.css" />
     <script src="http://cdnjs.cloudflare.com/ajax/libs/qtip2/2.1.1/jquery.qtip.js"></script>
-
+    <!--Accordion stuff-->
+    
     <!-- fonts -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <meta charset="UTF-8">
@@ -14,8 +15,8 @@
     <!--[if lt IE 9]>
         <script src="scripts/html5shiv.js"></script>
     <![endif]-->
-    <!--styles-->
     
+    <!--styles-->
     <link rel="stylesheet" href="css/style.css" />
     <link rel="stylesheet" href="css/about.css" />
     <link rel="stylesheet" href="css/matrix.css" />
