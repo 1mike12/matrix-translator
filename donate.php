3b6f59d32e54a23ecb7b2a0d79247de51b0ca976
Mike Qin <1mike12@gmail.com>
2014-01-18 21:52:54 -0500

    working on paypal column positioning. shit aint working
    todo:
    fix responsive on matrix page. widths broken again
    fix footer not goign to bottom on comments.php

--- donate.php
+++ donate.php
@@ -10,21 +10,15 @@
                     <div class="aboutSection selfClear">
                         <div id="donationWrapper">
                             <div id="donationLeft">
-                                <p>
-                                    We want to make our website a free service to everyone to use. We don't want ads to clutter up the simple interface. Consider a donation to help keep Matrix Translator up. 
-                                </p>
-                            </div>
-                            <div id='paypal'>
-                                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                                    <input type="hidden" name="cmd" value="_s-xclick">
-                                    <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHNwYJKoZIhvcNAQcEoIIHKDCCByQCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYC32d88pv+vZXyOYClvCx2tpMJ5M7zOSWd7vZBPTzU2mgow0Wzu07QiGl/RmKXAxPWhY4nTZiVBEpMAHuI/8TjhR4FnSH4a9QylHzKDgrCT8+mKXgZoIDJxXgFrHPjjuonYevADMx5ZSf4MawvxOvETMSq3KzKMrPynOVFIqwLW9zELMAkGBSsOAwIaBQAwgbQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQITDVyXIiHOUCAgZBeQ1lChscLZ+7SIfG8nwHdgGlEyI4gdKOareOZpHoaf5+jF98G+PPvafJZ7WHZqGT8/hAe0x+072NgejaiswAqaLaUJeRtczGQOjnmgsPuGkvvODEi8yg98JZGqmwMTyakreOPwyhJP64WN92bMmxTbOg4axiR4m80IEcxccly7/CpsIzLWR/n0t4FLUZMDjmgggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xNDAxMTgyMjA3NTBaMCMGCSqGSIb3DQEJBDEWBBSr6lbE+elERf/LwWyjd7my0yLBMjANBgkqhkiG9w0BAQEFAASBgAQbX9hsjcEzxKN5XMcoKJTPN9hfWM/9PZCnFZXvbK+Zs6Oo9ecHafxSwJUgYqD9sT5scdz+xxw/zZRPZI7Y1WCa6fwA3X2uBpKKBTxgHx4RXVG++MhV7Py8XwDJqKPzCT2Zmu4EvXmYYvJ8iXlo1nLdIrdmtDs/0yf8qEGBx2ZU-----END PKCS7-----
-                                           ">
-                                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-                                    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-                                </form>
-
+                                <div>
+                                    <p>
+                                        We want to make our website a free service to everyone to use. We don't want ads to clutter up the simple interface. Consider a donation to help keep Matrix Translator up. 
+                                    </p>
+                                </div>
                             </div>
                         </div>
+                        <div id='paypal'>paypal</div>
+
                     </div>
                     <h1>Spread the Word</h1>
                     <div class="aboutSection">
