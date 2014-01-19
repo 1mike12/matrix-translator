58e851d3514842b25ea9e31ab0ad08b2e762d722
Mike Qin <1mike12@gmail.com>
2014-01-19 17:34:38 -0500

    linkedin profiles about.php headers now are accordions minor div deletion
    todo:
    fix responsive on matrix page. widths broken again
    fix footer not goign to bottom on comments.php

--- about.php
+++ about.php
@@ -4,37 +4,49 @@
     <body>
         <?php include 'header.php' ?>
         <div id="wrapper">
-            <div class="section selfClear">
-                <div class="content">
-                    <h1>What is Matrix Translator?</h1>
-                    <div class="aboutSection">
-                        <h2>Matrix Translator automatically formats a matrix to be copied and pasted into 3rd party websites and programs.</h2>
-                        <p></p>
-                        <p>The inspiration for developing Matrix Translator came when Mike took a linear algebra class. He wasted a lot of time remembering the complex syntax between each of the programs and websites out there. Thus, the original idea for Matrix Translator was born!</p>
-                    </div>
-                    <h1 class="">How it's made</h1>
-                    <div class="aboutSection">
+            <div class="content">
+                <h1>What is Matrix Translator?</h1>
+                <div class="section">
+                    <h2>Matrix Translator automatically formats a matrix to be copied and pasted into 3rd party websites and programs.</h2>
+                    <p></p>
+                    <p>The inspiration for developing Matrix Translator came when Mike took a linear algebra class. He wasted a lot of time remembering the complex syntax between each of the programs and websites out there. Thus, the original idea for Matrix Translator was born!</p>
+                </div>
+                <h1 class="">How it's made</h1>
+                <div class="section">
+                    <div class="accordionHeaderWrapper">
+                        <div class="accordionHeaderIcon"></div>
                         <h3 class="accordionHeader">jQuery</h3>
-                        <div class="accordionContent">
-                            <p>Jquery is used heavily to enable functionality; from the sliding accordion effect that you see on this very page, to the actual code crunching going on behind the scenes. </p>
-                        </div>
-                        <h3 class="accordionHeader">Matrix Conversion</h3>
-                        <div class="accordionContent">
-                            <p>The matrix conversion algorithm was created using javscript and the jQuery language. When you click out of any of the fields, your browser automatically generates an internal array which represents the visual table that you see. Then, through a series of translation functions, the correct output is pieced together for each respective language.</p>
-                        </div>
+                    </div>
+                    <div class="accordionContent">
+                        <p>Jquery is used heavily to enable functionality; from the sliding accordion effect that you see on this very page, to the actual code crunching going on behind the scenes. </p>
                     </div>
 
-                    <h1>Developers</h1>
-                    <div class="aboutSection selfClear">
-                        <div style="float:left; width:49%;">
-                            <h2>Mike Qin</h2>
-                            <p>Portfolio | Linkedin</p>
-                            <p>Programming</p>
+                    <div class="accordionHeaderWrapper">
+                        <div class="accordionHeaderIcon"></div>
+                        <h3 class="accordionHeader">Matrix Conversion</h3>
+                    </div>
+                    <div class="accordionContent">
+                        <p>The matrix conversion algorithm was created using javscript and the jQuery language. When you click out of any of the fields, your browser automatically generates an internal array which represents the visual table that you see. Then, through a series of translation functions, the correct output is pieced together for each respective language.</p>
+                    </div>
+                </div>
+
+                <h1>Developers</h1>
+                <div class="section selfClear">
+                    <div style="float:left; width:50%;">
+                        <h2>Mike Qin</h2>
+                        <p>Programming</p>
+                        <div>
+                            <script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+                            <script type="IN/MemberProfile" data-id="http://www.linkedin.com/pub/mike-qin/13/941/96a" data-format="hover" data-related="false"></script>
                         </div>
-                        <div style="float:left; width:48%;">
-                            <h2>Brian Gioia</h2>
-                            <p>Portfolio | Linkedin</p>
-                            <p>User Interface</p>
+
+                    </div>
+                    <div style="float:left; width:50%;">
+                        <h2>Brian Gioia</h2>
+                        <p>User Interface</p>
+                        <div>
+                            <script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+                            <script type="IN/MemberProfile" data-id="www.linkedin.com/in/gioiabrian" data-format="hover" data-related="false"></script> | <a href="#">portfolio</a>
                         </div>
                     </div>
                 </div>
