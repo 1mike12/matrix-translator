cd3619e8e96eba4eb734e0bc3a82089bc4e20fd5
Mike Qin <1mike12@gmail.com>
2014-03-01 13:47:56 -0500

    accordion.js changed to revealing module

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,25 +1,66 @@
 $(document).ready(function() {
-    var contents = $(".accordionContent");
-    var headerWrapper = $(".accordionHeaderWrapper");
-    contents.hide();
+    var accordion = (function() {
+        //default header and content classes
+        var contentClass = ".accordionContent";
+        var headerClass = ".accordionHeaderWrapper";
 
-    headerWrapper.click(function() {
-        var adjacent = $(this).next();
-        if (adjacent.is(":hidden")) {
-            adjacent.slideDown("fast");
-            $(this).children().toggleClass("accordion-down");
-        } else {
-            $(this).next().slideUp("fast");
-            $(this).children().toggleClass("accordion-down");
+        //pluggin specific settings, should not normally override 
+        var settings = {
+            downClass: "accordionizer-down",
+            hoverClass: "accordionizer-hover",
+            slideSpeed: 200 //400 = "fast" in jquery
+        };
+
+        var $contents;
+        var $headers;
+
+        // can be empty, or init({contentClass: "asd", headerClass: "qwer"})
+        function init(options) {
+            if (typeof options === typeof {}) {
+                try {
+                    contentClass = options.contentClass;
+                    headerClass = options.headerClass;
+                }
+                catch (error) {
+                    console.log("error within accordionizer. Error message: " + error);
+                }
+            }
+            generateObjects();
+            $contents.hide();
+            attachClick();
+            attachHover();
         }
-    });
+        function generateObjects() {
+            $contents = $(contentClass);
+            $headers = $(headerClass);
+        }
+        function attachClick() {
+            $headers.click(function() {
+                var adjacent = $(this).next();
+                if (adjacent.is(":hidden")) {
+                    adjacent.slideDown(settings.slideSpeed);
+                    $(this).children().toggleClass(settings.downClass);
+                } else {
+                    $(this).next().slideUp(settings.slideSpeed);
+                    $(this).children().toggleClass(settings.downClass);
+                }
+            });
+        }
+        function attachHover() {
+            $headers.hover(
+                function() {
+                    $(this).toggleClass(settings.hoverClass);
+                    $(this).children().toggleClass(settings.hoverClass);
+                },
+                function() {
+                    $(this).toggleClass(settings.hoverClass);
+                    $(this).children().toggleClass(settings.hoverClass);
+                });
+        }
+        return{
+            init: init
+        };
+    })();
 
-    headerWrapper.hover(
-        function() {
-            $(this).toggleClass("accordion-hover");
-            $(this).children().toggleClass("accordion-hover");
-        },function(){
-            $(this).toggleClass("accordion-hover");
-            $(this).children().toggleClass("accordion-hover");
-        });
+    accordion.init();
 });
